cc19701350230da557b0dba6ce7443cd7ef43834
rhit-mirandm1 <95196506+rhit-mirandm1@users.noreply.github.com>
2023-09-22 19:07:43 -0400

    finished homework01

--- Homework/Homework01/scripts/main.js
+++ Homework/Homework01/scripts/main.js
@@ -1,21 +1,55 @@
-let counter =0;
+let counter = 0;
 
-updateView = function (){
+updateView = function () {
     document.querySelector("#counterText").innerHTML = `${counter}`
 }
-main = function (){
-document.querySelector("#minusButton").onclick = (event)=>{
-    counter = counter-1;
-    updateView();
+updateColor = function (color) {
+    document.getElementById('favoriteColorBox').style.backgroundColor = `${color}`;
 }
-document.querySelector("#resetButton").onclick = (event)=>{
-    counter = 0;
-    updateView();
+updateText = function (text) {
+    document.getElementById('favoriteColorBox').textContent =`${text}`;
 }
-document.querySelector("#plusButton").onclick = (event)=>{
-    counter = counter+1;
-    updateView();
-}
+
+
+main = function () {
+    document.querySelector("#minusButton").onclick = (event) => {
+        counter = counter - 1;
+        updateView();
+    }
+    document.querySelector("#resetButton").onclick = (event) => {
+        counter = 0;
+        updateView();
+    }
+    document.querySelector("#plusButton").onclick = (event) => {
+        counter = counter + 1;
+        updateView();
+    }
+    document.querySelector("#blueButton").onclick = (event) => {
+        color = getComputedStyle(document.getElementById("blueButton")).backgroundColor;
+        text =  document.getElementById("blueButton").textContent;
+        updateColor(color);
+        updateText(text);
+
+    }
+    document.querySelector("#redButton").onclick = (event) => {
+        color = getComputedStyle(document.getElementById("redButton")).backgroundColor;
+        text =  document.getElementById("redButton").textContent;
+        updateColor(color);
+        updateText(text);
+    }
+    document.querySelector("#greenButton").onclick = (event) => {
+        color = getComputedStyle(document.getElementById("greenButton")).backgroundColor;
+        text =  document.getElementById("greenButton").textContent;
+        updateColor(color);
+        updateText(text);
+    }
+    document.querySelector("#purpleButton").onclick = (event) => {
+        color = getComputedStyle(document.getElementById("purpleButton")).backgroundColor;
+        text =  document.getElementById("purpleButton").textContent;
+        updateColor(color);
+        updateText(text);
+    }
+    
 
 
 
